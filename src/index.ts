57093fd7c25a1af2eafb4a5e100908a97cafcd13
MAKOKHA-KWEYU-SADIKI <makokha.14460@students.kyu.ac.ke>
2024-06-06 11:55:13 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
-function f(nam:string){
-    console.log("ts configuration");
-}
-f("hello");
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app=new Hono()
+app.get('/',(c)=>{
+    return c.text("hello world")
+})
+app.get('/w',(c)=>{
+    return c.text("backend is interesting")
+})
+
+const port=3000
+serve({
+    fetch:app.fetch,
+    port
+})
+
+
+
+
+
+// function f(nam:string){
+//     console.log("ts configuration");
+// }
+// f("hello");
